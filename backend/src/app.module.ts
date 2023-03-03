12559d18fdcd48602b94c82c1150510773b594d2
Thang Le <jakethecat21@gmail.com>
2023-03-04 06:34:09 +0700

    Serve static conf

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,17 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from 'src/controllers/app.controller';
 import { AppService } from 'src/services/app.service';
+import { appEnv } from './configs/config';
 // import { ormConfig } from './configs/typeorm.config';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './modules/auth.module';
 import { FileModule } from './modules/file.module';
 import { UserModule } from './modules/user.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
 // import dataSource from 'src/configs/typeorm.config';
 
 @Module({
   imports: [
-    // TypeOrmModule.forRoot(ormConfig),
     DatabaseModule,
+    ServeStaticModule.forRoot({
+      rootPath: appEnv.HTML_CACHE_PATH,
+    }),
     UserModule,
     AuthModule,
     FileModule
